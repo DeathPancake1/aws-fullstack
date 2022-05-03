793b323d54771c2e9497f1b533730b82f54fea4b
Youssef Dawoud <41302314+DeathPancake1@users.noreply.github.com>
2022-05-03 12:54:44 +0200

    Update deploy.sh

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,4 +1,1 @@
-aws configure --profile default
-$AWS_ACCESS_KEY_ID
-$AWS_SECRET_ACCESS_KEY
-aws s3 cp --recursive --acl public-read ./www s3://dawoud123/
+aws s3 cp --recursive --acl public-read ./www s3://dawoud123/ --profile default
